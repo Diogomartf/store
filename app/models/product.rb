41cbfcb86bbeb0b6412725dd13b6683317970cf9
Diogo <diogo.mart.ferreira@gmail.com>
2021-10-03 01:17:26 +0100

    Add reviews to product page - add average rating method to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :reviews
+
+  def average_rating
+    return 0 if reviews.empty?
+    reviews.sum(:rating) / reviews.size.to_f
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "average rating when product has reviews" do
+    product = products(:zero_to_one)
+
+    assert product.average_rating == 4.0
+  end
+
+  test "average rating should be 0 when product has no reviews" do
+    product = products(:how_to_live)
+
+    assert product.average_rating == 0
+  end
 end
