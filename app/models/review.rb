fb426137e136437d98f1fa0be35b136d88823551
Diogo <diogo.mart.ferreira@gmail.com>
2021-10-10 01:35:30 +0100

    Add validations to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :rating, presence: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 5
+  }
+  validates :comment, length: {minimum: 2}
 end
